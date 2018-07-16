38f78b89c37c1deb42af94acd3b2318109cb0076
Scott Bellware <sbellware@gmail.com>
2018-07-16 17:05:39 -0500

    Aggregates are included in message store reporting

--- script/select-message-types.sh
+++ script/select-message-types.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
-psql message_store -U message_store -P pager=off -c "SELECT DISTINCT type FROM messages;"
+psql message_store -U message_store -P pager=off -c "SELECT DISTINCT type, count(type) FROM messages GROUP BY type ORDER BY count DESC;"
+
+# psql message_store -U message_store -P pager=off -c "SELECT DISTINCT stream_name, count(stream_name) FROM messages GROUP BY stream_name ORDER BY count DESC;"
+
+psql message_store -U message_store -P pager=off -c "SELECT count(*) FROM messages;"
--- script/select-stream-names.sh
+++ script/select-stream-names.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
-psql message_store -U message_store -P pager=off -c "SELECT DISTINCT stream_name FROM messages;"
+psql message_store -U message_store -P pager=off -c "SELECT DISTINCT stream_name, count(stream_name) FROM messages GROUP BY stream_name ORDER BY count DESC;"
+
+psql message_store -U message_store -P pager=off -c "SELECT count(*) FROM messages;"
