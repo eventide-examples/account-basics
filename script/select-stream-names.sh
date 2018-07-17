42367c7d4f3a7d3ba3e4943a4860e8a35999053c
Scott Bellware <sbellware@gmail.com>
2018-07-16 19:19:39 -0500

    Message store reporting is moved to the message store package

--- script/select-stream-names.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-psql message_store -U message_store -P pager=off -c "SELECT DISTINCT stream_name, count(stream_name) FROM messages GROUP BY stream_name ORDER BY count DESC;"
-
-psql message_store -U message_store -P pager=off -c "SELECT count(*) FROM messages;"
