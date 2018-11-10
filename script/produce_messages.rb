a445208f1d6818d44554242dc238ba44ad013d26
Scott Bellware <sbellware@gmail.com>
2018-11-10 14:40:12 -0600

    Message times increment to demo time line

--- script/produce_messages.rb
+++ script/produce_messages.rb
@@ -23,16 +23,27 @@ message_classes = [Deposit, Withdraw]
 account_ids = ['123', '456']
 amounts = [1, 11, 111]
 
+day = 0
+month = 1
+
 loop do
   message_class = message_classes.sample
   account_id = account_ids.sample
   amount = amounts.sample
-  time = Clock.iso8601
+
+  day = day + 1
+  time = Time.new(2000, month, day)
+  iso_time = Clock.iso8601(time)
+
+  if day == 28
+    month = month + 1
+    day = 0
+  end
 
   message = message_class.new
   message.account_id = account_id
   message.amount = amount
-  message.time = time
+  message.time = iso_time
 
   stream_name = "account:command-#{account_id}"
 
